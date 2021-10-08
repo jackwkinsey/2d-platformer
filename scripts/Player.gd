508d0cb86bcb210cf769cbd015fc35eb7a8caf52
Jack Kinsey <jack.w.kinsey@gmail.com>
2021-10-08 11:37:55 -0400

    feat: add basic player animation
    Idle, run, and jump. Also flip sprite based on move direction.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,10 +12,9 @@ func _ready():
 	pass
 
 func _process(delta):
-	var move_direction = Vector2.ZERO
-	move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	velocity.x += move_direction.x * horizontal_acceleration * delta
-	if (move_direction.x == 0):
+	var move_vector = _get_move_vector()
+	velocity.x += move_vector.x * horizontal_acceleration * delta
+	if (move_vector.x == 0):
 		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
 	
 	velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
@@ -33,7 +32,27 @@ func _process(delta):
 	else:
 		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	_update_animation(delta)
+
+func _get_move_vector():
+	var move_vector = Vector2.ZERO
+	move_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	return move_vector
 
 func _on_floor(delta):
 	air_time += delta
-	return is_on_floor() ||air_time < 0.15
+	return is_on_floor() || air_time < 0.2
+
+func _update_animation(delta):
+	var move_vector = _get_move_vector()
+	
+	if (!_on_floor(delta)):
+		$AnimatedSprite.play("jump")
+	elif (move_vector.x != 0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+
+	if (move_vector.x !=0):
+		$AnimatedSprite.flip_h = true if (move_vector.x > 0) else false
